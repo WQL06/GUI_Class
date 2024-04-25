cddd0958254a231b1002d6199e925d1b1ba5fea4
WQL06 <wes120166@gmail.com>
2024-04-25 21:31:24 +0800

    Ttry cath

---
+++ N09310018 GUI_Class/N09310018 GUI_Class/FormTryCath.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace N09310018_GUI_Class
+{
+    public partial class FormTryCath : Form
+    {
+        public FormTryCath()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double input = Double.Parse(textBox1.Text);
+                double sum = input + 100;
+                button1.Text = sum.ToString();
+            }
+            catch (System.FormatException)
+            {
+                MessageBox.Show("輸入格式有問題");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("發生問題了,什麼問題不知道");
+            }
+            finally
+            {
+                MessageBOX.Show("執行到最後");
+            }
+        }   
+
+
+
+    }
+}
